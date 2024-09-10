403986ebfb77013b8140b1e21fb129f6c7a78310
Umang Raval <umangraval0365@gmail.com>
2024-09-10 16:05:23 +0530

    modify it more

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -151,45 +151,46 @@ const handleToggle = (title) => {
       </section>
 
       {/* properties */}
-      <section className="px-4 md:px-8 lg:px-16 py-8 bg-gray-100">
-      <div className="bg-white py-16 xl:py-28 rounded-3xl shadow-lg">
-        <span className="text-xl font-medium text-gray-700">Your Future Home Awaits!</span>
-        <h2 className="text-2xl md:text-3xl font-bold mt-4 mb-6">Find Your Dream Here</h2>
-        <div className="flex justify-between items-center mb-6">
-          <h5 className="text-gray-600"><span>Showing 1-9</span> out of 3k properties</h5>
-          <Link to={'/'}><VscSettings className="text-gray-700 text-xl" /></Link>
-        </div>
-        {/* Swiper container */}
-        <Swiper
-          autoplay={{
-            delay: 4000,
-            disableOnInteraction: false,
-          }}
-          breakpoints={{
-            600: {
-              slidesPerView: 2,
-              spaceBetween: 30,
-            },
-            1124: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-            1300: {
-              slidesPerView: 4,
-              spaceBetween: 30,
-            },
-          }}
-          modules={[Autoplay]}
-          className="h-[488px] md:h-[533px] mt-5"
-        >
-          {PROPERTIES.map((property) => (
-            <SwiperSlide key={property.title}>
-              <Item property={property} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
+      
+      <section className="px-0 md:px-0 lg:px-0 py-0 bg-white">
+  <div className="py-16 xl:py-28">
+    <span className="text-xl font-medium text-gray-700">Your Future Home Awaits!</span>
+    <h2 className="text-2xl md:text-3xl font-bold mt-4 mb-6">Find Your Dream Here</h2>
+    <div className="flex justify-between items-center mb-6">
+      <h5 className="text-gray-600"><span>Showing 1-9</span> out of 3k properties</h5>
+      <Link to={'/'}><VscSettings className="text-gray-700 text-xl" /></Link>
+    </div>
+    {/* Swiper container */}
+    <Swiper
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1124: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        1300: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      }}
+      modules={[Autoplay]}
+      className="h-[488px] md:h-[533px] mt-5"
+    >
+      {PROPERTIES.map((property) => (
+        <SwiperSlide key={property.title}>
+          <Item property={property} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </div>
+</section>
 
 
     {/* Blog */}
