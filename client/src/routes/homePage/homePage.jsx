4cc278645cbe0010e42ace57dbbda03acbc119dc
Umang Raval <umangraval0365@gmail.com>
2024-09-16 14:00:30 +0530

    minor changes 16/09/24

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -195,7 +195,7 @@ const handleToggle = (title) => {
 
 
     {/* Blog */}
-    <section className='max-padd-container py-16 xl:py-28'>
+    <section className="max-padd-container py-16 xl:py-28">
             <h2 className='text-4xl font-bold text-center mb-12'>Our Expert Blogs</h2>
             <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                 {BLOGS.map((blog) => (
