2dac6e8345b9bec5fd5325c11f8260240f9993c9
Umang Raval <umangraval0365@gmail.com>
2024-10-16 13:35:08 +0530

    Post Page completed,  new page backend and frontend

--- api/app.js
+++ api/app.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
-app.listen(8800,() => {
-    console.log('Server is running ');
- });
+app.listen(8800, () => {
+  console.log("Server is running ");
+});
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,18 +1,13 @@
-import express from 'express';
+import express from "express";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-// router.get("/test", (req, res) => {
-//     console.log("router  is working");
-// });
-// router.post("/test", (req, res) => {
-//     console.log("router  is working");
-// });
-// router.put("/test", (req, res) => {
-//     console.log("router  is working");
-// });
-// router.delete("/test", (req, res) => {
-//     console.log("router  is working");
-// });
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
