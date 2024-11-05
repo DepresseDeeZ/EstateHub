aec7ec824d5883193e8701835d73fe610e3e94be
Umang Raval <umangraval0365@gmail.com>
2024-11-05 16:42:44 +0530

    api updated

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8800/api/",
-  baseURL: "https://estate-hub-back-depressedeezs-projects.vercel.app/api/",
+  baseURL: "https://realestate-website-1.onrender.com/api/",
   withCredentials: true,
 });
 
