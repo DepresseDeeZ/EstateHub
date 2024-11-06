734a52113e237b8e4ff63076aba1470b5c1815c6
Umang Raval <umangraval0365@gmail.com>
2024-11-06 22:59:11 +0530

    changes 2

--- api/app.js
+++ api/app.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://estate-hub-omega.vercel.app");
   next();
 });
 app.use(
