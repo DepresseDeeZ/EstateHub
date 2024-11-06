4a4f76b2a6c5771995dc819c1ed1732ea9593d2d
Umang Raval <umangraval0365@gmail.com>
2024-11-06 22:48:58 +0530

    upload

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "http://localhost:8800/api/",
-  baseURL: "estate-hub-back-i4jszqeli-depressedeezs-projects.vercel.app/api/",
+  baseURL: "http://localhost:8800/api/",
+  // baseURL: "estate-hub-back-i4jszqeli-depressedeezs-projects.vercel.app/api/",
   withCredentials: true,
 });
 
