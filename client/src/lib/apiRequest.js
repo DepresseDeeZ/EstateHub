56154c25f10b0c1b82720921fdc90b6dcb156c26
Umang Raval <umangraval0365@gmail.com>
2024-11-06 23:02:56 +0530

    send upate

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8800/api/",
-  baseURL:
-    "https://estate-hub-kwlq.vercel.app/?vercelToolbarCode=mjp2CMu5I0qqSDB/api/",
+  baseURL: "https://estate-hub-back.vercel.app/api/",
   withCredentials: true,
 });
 
